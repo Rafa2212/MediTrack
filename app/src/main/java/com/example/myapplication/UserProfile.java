94d9f487868975ca2783b915337c1e23d903ddc5
Rafael <rafael.tot@syonic.eu>
2024-04-06 11:54:16 +0300

    userProfile

---
+++ app/src/main/java/com/example/myapplication/UserProfile.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+public class UserProfile {
+    private String name;
+    private int age;
+    private float height;
+    private float weight;
+
+    public UserProfile(String name, int age, float height, float weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+}
